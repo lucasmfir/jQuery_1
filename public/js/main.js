6e504101bc03b55e34849f6f4be3f02633976b6e
Lucas Firmo <lucasmfir@gmail.com>
2017-01-12 19:04:25 -0200

    events

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,30 @@
 var frase = $(".frase").text();
-
 var numeroPalavras = frase.split(" ").length;
-
 $("#nPalavras").text(numeroPalavras);
+
+var campo = $(".campo-digitacao");
+
+campo.on("input", function(){
+	var conteudo = campo.val();
+	var contPalavras = conteudo.split(/\S+/).length -1;
+	$("#contador-palavras").text(contPalavras);
+
+	var contCaracteres = conteudo.length;
+	$("#contador-caracteres").text(contCaracteres);
+});
+
+var tRestante = $("#tDigitacao").text();
+
+campo.one("focus", function(){
+	var cronometroID = setInterval(function(){
+		tRestante--;
+		console.log(tRestante);
+		$("#tDigitacao").text(tRestante);
+		if(tRestante < 1){
+			clearInterval(cronometroID);
+			campo.attr("disabled", true);
+		}
+	},1000);
+
+});
+
